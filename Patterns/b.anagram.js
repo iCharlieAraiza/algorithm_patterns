50f98b1bcd745cccfb0b694f8f1397f4df5c4775
iCharlieAraiza <eliptikman1234@gmail.com>
2018-10-24 12:58:31 -0500

    Adding anagram algorithm examples

---
+++ Patterns/b.anagram.js
@@ -0,0 +1,47 @@
+/**
+* Anagram cheker using frequency pattern
+*
+**/
+
+/*
+
+word1 = 'casa'
+word2 = 'saca'
+
+anagram(word1,word2) // true
+
+
+*/
+
+function anagram(str, str2){
+
+    if(str.length !== str2.length){
+        return false;
+    }
+        const word1 ={};
+        
+        for(let val of str){
+            word1[val] = (word1[val]>0)?word1[val]+1: 1;
+        }
+
+        for(let val of str2){
+            
+            //Zero doesn't exist
+            if(!word1[val]){
+                return false;
+            }else{
+                word1[val]=word1[val]-1;
+            }
+        }
+        
+        return true;
+
+}
+
+console.log(anagram('casatp','spacya'));
+
+
+
+
+
+//Comprobar que cuenta cada uno de los elmentos coincida
