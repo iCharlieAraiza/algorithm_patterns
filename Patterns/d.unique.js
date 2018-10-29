fbf1476af7c742c7c75a0af8837bcaf9f77605a9
iCharlieAraiza <eliptikman1234@gmail.com>
2018-10-28 20:48:14 -0600

    Improved the code of the Count Unique Algorithm

--- Patterns/d.unique.js
+++ Patterns/d.unique.js
@@ -10,28 +10,30 @@ function countUniqueValues(array){
     }
 
 
-    let left =0,
-        right= 1,
-        inter= 0;
-    //While: right <= array.length
-    while(right <= array.length){
-        inter++;
-        if(array[left] === array[right]){
-            
-            //console.log(inter,'Right (val): ',array[right], 'Right (pointer): ',right);
-            right++;
-        }else {
-            
-            //console.log(inter,'Left (val): ',array[left],'Right (val): ',array[right], 'Left (pointer): ',left)
-            left++;
-            array[left] = array[right];
+    let j= 0;
+    
+    for(i=0; i<array.length; i++){
+        if(array[i] !== array[j]){
+            j++;
+            array[j] = array[i];
         }
     }
-    //->If: array[left] === array[right] : right++
-    //<-Else: array[left] !== array[right] : left++
-    return left;
+    
+    // while(right <= array.length){
+    //     if(array[left] === array[right]){
+            
+    //         //console.log(inter,'Right (val): ',array[right], 'Right (pointer): ',right);
+    //         right++;
+    //     }else {
+            
+    //         //console.log(inter,'Left (val): ',array[left],'Right (val): ',array[right], 'Left (pointer): ',left)
+    //         left++;
+    //         array[left] = array[right];
+    //     }
+    // }
+    return j+1;
 } 
 
-console.log(countUniqueValues([-2, -1, -1, 1, 1, 1, 1, 2, 4, 4, 6, 6, 6, 7, 8, 9, 9, 9, 10 ]) );  // 9
+console.log(countUniqueValues([-2, -2, -1, -1, 0, 1, 1, 1, 1, 2, 4, 4, 6, 6, 6, 7, 8, 9, 9, 9, 10, 10  ]) );  // 9
 
 //countUniqueValues([1, 1, 1, 1, 1, 4, 4, 6]) // 3
