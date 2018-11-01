734a88c3e6a2e06b5d932559730d7d418c8e4018
iCharlieAraiza <eliptikman1234@gmail.com>
2018-11-01 14:31:25 -0600

    Fixing some issues

--- Patterns/e.sliding.js
+++ Patterns/e.sliding.js
@@ -14,42 +14,34 @@
 //  función que encuentra un patrón dentro del array
 
  const sumElement = (array, el)=>{
-    //Verificar que el largo del array sea mayor o igual a el => retrurn false.
     if(array.length < el){
         return null;
     }
     
-    //Declarar valor suma, max, index, resultArray.
-    let sum = 0,
-          max = -Infinity,
-          index = 0;
+    let sum = 0,max = -Infinity, index = 0;
 
     const resultArray = [];
 
-    //!= For recorrer el array de i<=el
+    //Fist sum
     for(let i=0; i<=el-1; i++){
-    //->suma = suma + array[i]
         sum = sum + array[i]
     }
-    // max = suma;
+
     max = sum;
 
-    //!= For el+1 <= array.lenght
+    // Checking the max value
     for(i=el; i<= array.length; i++){
-    //->suma->suma = suma + array[i] - array[i-3]
         sum = sum + array[i] - array[i-el];
-    //->!= If: suma > max ? max = suma, index = i
-        console.log("Elemento i", i, "Suma ", sum);
+        
         if(sum>max){
             max = sum;
             index = i-el+1;
         }
     }
 
-    //!= For i=index; i<index+el; i++
+    //Create the new Array
     for(i= index; i<index+el; i++){
-    //->push en resultArray el array[i] 
-    resultArray.push(array[i])
+        resultArray.push(array[i])
     }
 
     return resultArray;
