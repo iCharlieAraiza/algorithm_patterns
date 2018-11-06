3cd3b4a34d123f246423d5277f3916e8a8a8a941
iCharlieAraiza <eliptikman1234@gmail.com>
2018-11-06 11:58:53 -0600

    Added new coments

--- Patterns/e.sliding.js
+++ Patterns/e.sliding.js
@@ -11,18 +11,21 @@
  **/
 
 
-//  función que encuentra un patrón dentro del array
-
+/**
+ * Sum Function
+ * 
+ * @param: array[num], number
+ * @returns: array[num] || Null 
+ * 
+ **/
  const sumElement = (array, el)=>{
     if(array.length < el){
         return null;
     }
     
     let sum = 0,max = -Infinity, index = 0;
-
     const resultArray = [];
 
-    //Fist sum
     for(let i=0; i<=el-1; i++){
         sum = sum + array[i]
     }
@@ -32,11 +35,12 @@
     // Checking the max value
     for(i=el; i<= array.length; i++){
         sum = sum + array[i] - array[i-el];
-        
+
         if(sum>max){
             max = sum;
             index = i-el+1;
         }
+
     }
 
     //Create the new Array
